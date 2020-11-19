d5435dc15433006d987efeae8c409a7a1d473e73
敖昕妍 <aoxinyan@tezign.com>
2020-11-19 12:13:50 +0800

    0-1 背包代码整理

--- src/knapsack.js
+++ src/knapsack.js
@@ -2,11 +2,15 @@
  * 0-1 背包问题
  * 给定一个固定大小、能够携重量 W 的背包，以及一组有价值和重量的物品.
  * 找出一个最佳解决方案，使得装入背包的物品总重量不超过 W，且总价值最大。
+ * 
+ * tips: 
+ *   Array.fill(param) 如果传入的参数是对象类型（包括对象字面量），那么数组所有元素是同一个对象引用
+ * 
  * @param {number} capacity 总重量
  * @param {Object[]} objects 物品
  * @param {number} objects[].value 物品价值
  * @param {number} objects[].weight 物品重量
- * @return {Object[]}
+ * @return {Object}
  */
 function knapsack(capacity, objects) {
   if (capacity < 0) {
@@ -14,11 +18,10 @@ function knapsack(capacity, objects) {
   }
 
   if (capacity === 0) {
-    return [];
+    return createSolution([]);
   }
 
-  const solutions = Array.from(Array(objects.length), () => []);
-  const totalValues = Array.from(Array(objects.length), () => []);
+  const solutions = Array.from(Array(objects.length), () => createSolution([]));
 
   for (let i = objects.length - 1; i >= 0; i--) {
     for (let c = 0; c <= capacity; c++) {
@@ -26,26 +29,31 @@ function knapsack(capacity, objects) {
       const knapsackNotFull = remainingCapacity >= 0;
 
       if (c === 0 || i === objects.length - 1) {
-        solutions[i][c] = knapsackNotFull ? [objects[i]] : [];
-        totalValues[i][c] = knapsackNotFull ? objects[i].value : 0;
+        solutions[i][c] = knapsackNotFull ? createSolution([objects[i]]) : createSolution([]);
       } else if (knapsackNotFull) {
-        const planA = [objects[i], ...solutions[i + 1][remainingCapacity]];
-        const planAValue = objects[i].value + totalValues[i + 1][remainingCapacity];
-        const planB = solutions[i + 1][c];
-        const planBValue = totalValues[i + 1][c];
-        const planABetter = planAValue > planBValue;
-
-        solutions[i][c] = planABetter ? planA : planB;
-        totalValues[i][c] = planABetter ? planAValue : planBValue;
+        const shouldCollect = solutions[i + 1][remainingCapacity].totalValue + objects[i].value > solutions[i + 1][c].totalValue;
+        solutions[i][c] = shouldCollect ? collectSolution(objects[i], solutions[i + 1][remainingCapacity]) : solutions[i + 1][c];
       } else {
         solutions[i][c] = solutions[i + 1][c];
-        totalValues[i][c] = totalValues[i + 1][c];
       }
     }
   }
   return solutions[0][capacity];
 }
 
+function createSolution(objects) {
+  return {
+    solution: objects,
+    totalValue: objects.reduce((prev, curr) => prev + curr.value, 0)
+  };
+}
+
+function collectSolution(object, collection) {
+  return {
+    solution: [object, ...collection.solution],
+    totalValue: object.value + collection.totalValue
+  }
+}
 const objects = [{value: 3, weight: 2}, {value: 4, weight: 3}, {value: 5, weight: 4}];
-const capacity = 6;
+const capacity = 4;
 console.log(knapsack(capacity, objects));
