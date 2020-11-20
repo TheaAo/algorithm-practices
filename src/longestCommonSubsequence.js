030c5af5a499cf371e5ad6cbd2aa90776f0631e3
敖昕妍 <aoxinyan@tezign.com>
2020-11-20 18:24:29 +0800

    leetcode 1143.最长公共子序列(DFS + cache)

---
+++ src/longestCommonSubsequence.js
@@ -0,0 +1,57 @@
+/**
+ * leetcode 1143.最长公共子序列
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+ * 一个字符串的 子序列 是指这样一个新的字符串：
+ * 它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+ * 若这两个字符串没有公共子序列，则返回 0。
+ * @param {string} str1 
+ * @param {string} str2 
+ * @return {number}
+ */
+
+/**
+ * DFS + cache
+ */
+function longestCommonSubsequence(str1, str2) {
+  if (!str1 || !str2) return 0;
+
+  if (str2.indexOf(str1) > -1) return str1.length;
+  if (str1.indexOf(str2) > -1) return str2.length;
+
+  const cache = [];
+  const str1Arr = str1.split("");
+
+  function getMaxLength(index1, index2) {
+    const substr2 = str2.substr(index2);
+    let maxLength = 0;
+
+    if (cache[index1] && cache[index1][index2]) {
+      return cache[index1][index2];
+    }
+    for(let i = index1; i < str1.length; i++) {
+      const char = str1Arr[i];
+      const newIndex2 = substr2.indexOf(char);
+      if (newIndex2 > -1) {
+        const length = 1 + getMaxLength(i + 1, newIndex2 + index2 + 1);
+        if (length > maxLength) {
+          maxLength = length;
+        }
+      }
+    }
+    cache[index1] = [];
+    cache[index1][index2] = maxLength; 
+    return maxLength;
+  }
+  return getMaxLength(0, 0);
+}
+
+/**
+ * DP
+ */
+
+const text1 = "fcvafurqjylclorwfoladwfqzkbebslwnmpmlkbezkxoncvwhstwzwpqxqtyxozkpgtgtsjobujezgrkvevklmludgtyrmjaxyputqbyxqvupojutsjwlwluzsbmvyxifqtglwvcnkfsfglwjwrmtyxmdgjifyjwrsnenuvsdedsbqdovwzsdghclcdexmtsbexwrszihcpibwpidixmpmxshwzmjgtadmtkxqfkrsdqjcrmxkbkfoncrcvoxuvcdytajgfwrcxivixanuzerebuzklyhezevonqdsrkzetsrgfgxibqpmfuxcrinetyzkvudghgrytsvwzkjulmhanankxqfihenuhmfsfkfepibkjmzybmlkzozmluvybyzsleludsxkpinizoraxonmhwtkfkhudizepyzijafqlepcbihofepmjqtgrsxorunshgpazovuhktatmlcfklafivivefyfubunszyvarcrkpsnglkduzaxqrerkvcnmrurkhkpargvcxefovwtapedaluhclmzynebczodwropwdenqxmrutuhehadyfspcpuxyzodifqdqzgbwhodcjonypyjwbwxepcpujerkrelunstebopkncdazexsbezmhynizsvarafwfmnclerafejgnizcbsrcvcnwrolofyzulcxaxqjqzunedidulspslebifinqrchyvapkzmzwbwjgbyrqhqpolwjijmzyduzerqnadapudmrazmzadstozytonuzarizszubkzkhenaxivytmjqjgvgzwpgxefatetoncjgjsdilmvgtgpgbibexwnexstipkjylalqnupexytkradwxmlmhsnmzuxcdkfkxyfgrmfqtajatgjctenqhkvyrgvapctqtyrufcdobibizihuhsrsterozotytubefutaxcjarknynetipehoduxyjstufwvkvwvwnuletybmrczgtmxctuny";
+const text2 = "nohgdazargvalupetizezqpklktojqtqdivcpsfgjopaxwbkvujilqbclehulatshehmjqhyfkpcfwxovajkvankjkvevgdovazmbgtqfwvejczsnmbchkdibstklkxarwjqbqxwvixavkhylqvghqpifijohudenozotejoxavkfkzcdqnoxydynavwdylwhatslyrwlejwdwrmpevmtwpahatwlaxmjmdgrebmfyngdcbmbgjcvqpcbadujkxaxujudmbejcrevuvcdobolcbstifedcvmngnqhudixgzktcdqngxmruhcxqxypwhahobudelivgvynefkjqdyvalmvudcdivmhghqrelurodwdsvuzmjixgdexonwjczghalsjopixsrwjixuzmjgxydqnipelgrivkzkxgjchibgnqbknstspujwdydszohqjsfuzstyjgnwhsrebmlwzkzijgnmnczmrehspihspyfedabotwvwxwpspypctizyhcxypqzctwlspszonsrmnyvmhsvqtkbyhmhwjmvazaviruzqxmbczaxmtqjexmdudypovkjklynktahupanujylylgrajozobsbwpwtohkfsxeverqxylwdwtojoxydepybavwhgdehafurqtcxqhuhkdwxkdojipolctcvcrsvczcxedglgrejerqdgrsvsxgjodajatsnixutihwpivihadqdotsvyrkxehodybapwlsjexixgponcxifijchejoxgxebmbclczqvkfuzgxsbshqvgfcraxytaxeviryhexmvqjybizivyjanwxmpojgxgbyhcruvqpafwjslkbohqlknkdqjixsfsdurgbsvclmrcrcnulinqvcdqhcvwdaxgvafwravunurqvizqtozuxinytafopmhchmxsxgfanetmdcjalmrolejidylkjktunqhkxchyjmpkvsfgnybsjedmzkrkhwryzan";
+
+console.log(longestCommonSubsequence(text1, text2));
